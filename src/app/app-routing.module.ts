0d4bb182066a4d5d5e3bacc9d8ea7977bbb5ba0c
WavePPG <65160143@go.buu.ac.th>
2024-06-06 15:24:49 +0700

    Add new components for about us, contact, features, footer, hero section, home, navbar, recipes, and testimonials
    - Updated app-routing.module.ts and app.module.ts
    - Modified app.component.html
    - Added new components: about-us, contact, features, footer, hero-section, home, navbar, recipes, and testimonials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect unknown routes to home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroSectionComponent,
+    FeaturesComponent,
+    AboutUsComponent,
+    TestimonialsComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
